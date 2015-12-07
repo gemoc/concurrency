2159b181bbfeca838ecedc858600b21e5d428c1c
Dorian Leroy <dorian.leroy@irisa.fr>
2015-12-07 16:41:41 +0100

    Make use of the variables view functionnality that was moved in AbstractGemocDebugger

--- executionengine/ccsljava/plugins/org.gemoc.gemoc_modeling_workbench.concurrent.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/debug/GemocModelDebugger.java
+++ executionengine/ccsljava/plugins/org.gemoc.gemoc_modeling_workbench.concurrent.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/debug/GemocModelDebugger.java
@@ -1,17 +1,21 @@
 package org.gemoc.gemoc_modeling_workbench.ui.debug;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.gemoc.execution.engine.core.AbstractExecutionEngine;
 import org.gemoc.execution.engine.core.EngineStoppedException;
 import org.gemoc.execution.engine.debug.AbstractGemocDebugger;
+import org.gemoc.execution.engine.debug.AnnotationMutableFieldExtractor;
+import org.gemoc.execution.engine.debug.IMutableFieldExtractor;
 import org.gemoc.execution.engine.debug.ui.breakpoint.GemocBreakpoint;
 import org.gemoc.execution.engine.trace.LogicalStepHelper;
 import org.gemoc.execution.engine.trace.gemoc_execution_trace.LogicalStep;
 import org.gemoc.execution.engine.trace.gemoc_execution_trace.MSEOccurrence;
-import org.gemoc.gemoc_language_workbench.api.core.EngineStatus.RunStatus;
+import org.gemoc.executionengine.ccsljava.concurrent_xdsml.ConcurrentLanguageDefinition;
+import org.gemoc.executionframework.xdsml_base.LanguageDefinition;
 import org.gemoc.gemoc_language_workbench.api.core.IBasicExecutionEngine;
 import org.gemoc.gemoc_language_workbench.api.engine_addon.IEngineAddon;
 
@@ -23,11 +27,6 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 	 * A fake instruction to prevent the stepping return to stop on each event.
 	 */
 	private static final EObject FAKE_INSTRUCTION = EcorePackage.eINSTANCE;
-
-	/**
-	 * The {@link NonDeterministicExecutionEngine} to debug.
-	 */
-	private final IBasicExecutionEngine engine;
 
 	/**
 	 * Tells if the logical step level stack frame is created.
@@ -43,10 +42,9 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 	 * Tells if we should break at the next {@link LogicalStep}.
 	 */
 	private boolean breakNextLogicalStep;
-	
+
 	public GemocModelDebugger(IDSLDebugEventProcessor target, IBasicExecutionEngine engine) {
-		super(target);
-		this.engine = engine;
+		super(target, engine);
 	}
 
 	@Override
@@ -62,7 +60,7 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 	@Override
 	public boolean canStepInto(String threadName, EObject instruction) {
 		final boolean res;
-		
+
 		if (instruction instanceof LogicalStep) {
 			boolean hasActionMSE = false;
 			for (MSEOccurrence mseOccurence : ((LogicalStep) instruction).getMseOccurrences()) {
@@ -75,12 +73,12 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 		} else {
 			res = false;
 		}
-		
+
 		return res;
 	}
 
 	@Override
-	public void updateData(String threadName, EObject instruction) {
+	protected void updateStack(String threadName, EObject instruction) {
 		if (mseFrameCreated) {
 			popStackFrame(threadName);
 			mseFrameCreated = false;
@@ -90,24 +88,18 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 			logicalStepFrameCreated = false;
 		}
 		if (instruction instanceof LogicalStep) {
-			pushStackFrame(threadName, LogicalStepHelper.getLogicalStepName((LogicalStep) instruction), instruction, instruction);
+			pushStackFrame(threadName, LogicalStepHelper.getLogicalStepName((LogicalStep) instruction), instruction,
+					instruction);
 			logicalStepFrameCreated = true;
 		} else if (instruction instanceof MSEOccurrence) {
 			final LogicalStep logicalStep = ((MSEOccurrence) instruction).getLogicalstep();
 			pushStackFrame(threadName, LogicalStepHelper.getLogicalStepName(logicalStep), logicalStep, logicalStep);
 			logicalStepFrameCreated = true;
 			pushStackFrame(threadName, instruction.toString(), instruction, instruction);
-			mseFrameCreated = true;				
+			mseFrameCreated = true;
 		}
-
-		// TODO populate variables...
-		// variable(threadName, instruction.eClass().getName(), "instruction",
-		// instruction);
 	}
-	
-	
-	
-	
+
 	@Override
 	public boolean shouldBreak(EObject instruction) {
 		boolean res = false;
@@ -121,41 +113,54 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 
 		return res;
 	}
-	
+
 	private boolean shouldBreakLogicalStep(LogicalStep logicalStep) {
-		
+
 		final boolean res;
-		
-		if ((super.shouldBreak(logicalStep) && Boolean.valueOf((String)getBreakpointAttributes(logicalStep, GemocBreakpoint.BREAK_ON_LOGICAL_STEP)))) {
+
+		if ((super.shouldBreak(logicalStep) && Boolean.valueOf((String) getBreakpointAttributes(logicalStep,
+				GemocBreakpoint.BREAK_ON_LOGICAL_STEP)))) {
 			res = true;
 		} else {
 			boolean hasMSEBreak = false;
 			for (MSEOccurrence mseOccurrence : logicalStep.getMseOccurrences()) {
-				hasMSEBreak = (super.shouldBreak(mseOccurrence.getMse()) && Boolean.valueOf((String)getBreakpointAttributes(mseOccurrence.getMse(), GemocBreakpoint.BREAK_ON_LOGICAL_STEP)));
-				hasMSEBreak = hasMSEBreak || (mseOccurrence.getMse().getCaller() != null && super.shouldBreak(mseOccurrence.getMse().getCaller()) && Boolean.valueOf((String)getBreakpointAttributes(mseOccurrence.getMse().getCaller(), GemocBreakpoint.BREAK_ON_LOGICAL_STEP)));
-				hasMSEBreak = hasMSEBreak || shouldBreakPredicates(engine, mseOccurrence);				
+				hasMSEBreak = (super.shouldBreak(mseOccurrence.getMse()) && Boolean
+						.valueOf((String) getBreakpointAttributes(mseOccurrence.getMse(),
+								GemocBreakpoint.BREAK_ON_LOGICAL_STEP)));
+				hasMSEBreak = hasMSEBreak
+						|| (mseOccurrence.getMse().getCaller() != null
+								&& super.shouldBreak(mseOccurrence.getMse().getCaller()) && Boolean
+									.valueOf((String) getBreakpointAttributes(mseOccurrence.getMse().getCaller(),
+											GemocBreakpoint.BREAK_ON_LOGICAL_STEP)));
+				hasMSEBreak = hasMSEBreak || shouldBreakPredicates(engine, mseOccurrence);
 				if (hasMSEBreak) {
 					break;
 				}
 			}
 			res = hasMSEBreak;
 		}
-		
+
 		return res;
 	}
-	
+
 	private boolean shouldBreakMSEOccurence(MSEOccurrence mseOccurrence) {
 		final boolean res;
-		
+
 		if (shouldBreakPredicates(engine, mseOccurrence))
 			res = true;
-		else if ((super.shouldBreak(mseOccurrence.getMse()) && Boolean.valueOf((String)getBreakpointAttributes(mseOccurrence.getMse(), GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE))) || (mseOccurrence.getMse().getCaller() != null && super.shouldBreak(mseOccurrence.getMse().getCaller()) && Boolean.valueOf((String)getBreakpointAttributes(mseOccurrence.getMse().getCaller(), GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE)))) {
+		else if ((super.shouldBreak(mseOccurrence.getMse()) && Boolean.valueOf((String) getBreakpointAttributes(
+				mseOccurrence.getMse(), GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE)))
+				|| (mseOccurrence.getMse().getCaller() != null && super.shouldBreak(mseOccurrence.getMse().getCaller()) && Boolean
+						.valueOf((String) getBreakpointAttributes(mseOccurrence.getMse().getCaller(),
+								GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE)))) {
 			res = true;
 		} else {
 			LogicalStep locicalStep = mseOccurrence.getLogicalstep();
-			res = super.shouldBreak(locicalStep) && Boolean.valueOf((String)getBreakpointAttributes(locicalStep, GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE));
+			res = super.shouldBreak(locicalStep)
+					&& Boolean.valueOf((String) getBreakpointAttributes(locicalStep,
+							GemocBreakpoint.BREAK_ON_MSE_OCCURRENCE));
 		}
-		
+
 		return res;
 	}
 
@@ -175,112 +180,51 @@ public class GemocModelDebugger extends AbstractGemocDebugger implements IEngine
 	}
 
 	@Override
-	public void engineAboutToStart(IBasicExecutionEngine engine) 
-	{
-	}
-	
-	@Override
-	public void engineStarted(IBasicExecutionEngine executionEngine) 
-	{
-		spawnRunningThread(Thread.currentThread().getName(), engine.getExecutionContext().getResourceModel().getContents().get(0));
-	}
-
-
-	@Override
-	public void aboutToSelectLogicalStep(IBasicExecutionEngine engine, Collection<LogicalStep> logicalSteps) 
-	{
+	public void engineStarted(IBasicExecutionEngine executionEngine) {
+		spawnRunningThread(Thread.currentThread().getName(), engine.getExecutionContext().getResourceModel()
+				.getContents().get(0));
 	}
 
 	@Override
-	public void logicalStepSelected(IBasicExecutionEngine engine, LogicalStep selectedLogicalStep) 
-	{
-	}
-
-	@Override
-	public void engineStopped(IBasicExecutionEngine engine) 
-	{
-		if (!isTerminated(Thread.currentThread().getName())) 
-		{
+	public void engineStopped(IBasicExecutionEngine engine) {
+		if (!isTerminated(Thread.currentThread().getName())) {
 			terminated(Thread.currentThread().getName());
 		}
 	}
 
 	@Override
-	public void aboutToExecuteLogicalStep(IBasicExecutionEngine executionEngine, LogicalStep logicalStepToApply) 
-	{
-		if (!control(((AbstractExecutionEngine)executionEngine).thread.getName(), logicalStepToApply))
-		{
+	public void aboutToExecuteLogicalStep(IBasicExecutionEngine executionEngine, LogicalStep logicalStepToApply) {
+		if (!control(((AbstractExecutionEngine) executionEngine).thread.getName(), logicalStepToApply)) {
 			throw new EngineStoppedException("Debug thread has stopped.");
 		}
 	}
 
 	@Override
-	public void aboutToExecuteMSEOccurrence(IBasicExecutionEngine executionEngine, MSEOccurrence mseOccurrence) 
-	{
-		if (!control(((AbstractExecutionEngine)executionEngine).thread.getName(), mseOccurrence))
-		{
-			throw new EngineStoppedException("Debug thread has stopped.");			
+	public void aboutToExecuteMSEOccurrence(IBasicExecutionEngine executionEngine, MSEOccurrence mseOccurrence) {
+		if (!control(((AbstractExecutionEngine) executionEngine).thread.getName(), mseOccurrence)) {
+			throw new EngineStoppedException("Debug thread has stopped.");
 		}
 	}
-	
+
 	@Override
-	public void terminate() 
-	{
+	public void terminate() {
 		super.terminate();
 		engine.stop();
 	}
 
 	@Override
-	public void engineStatusChanged(IBasicExecutionEngine engineRunnable, RunStatus newStatus) 
-	{
+	protected List<IMutableFieldExtractor> getMutableFieldExtractors() {
+		List<IMutableFieldExtractor> extractors = new ArrayList<IMutableFieldExtractor>();
+		extractors.add(new AnnotationMutableFieldExtractor());
+		return extractors;
 	}
-
+	
 	@Override
-	public void engineAboutToStop(IBasicExecutionEngine engine) 
-	{
-	}
-
-	@Override
-	public void logicalStepExecuted(IBasicExecutionEngine engine, LogicalStep logicalStepExecuted) 
-	{
-	}
-
-	@Override
-	public void mseOccurrenceExecuted(IBasicExecutionEngine engine, MSEOccurrence mseOccurrence) 
-	{
-	}
-
-	@Override
-	public void proposedLogicalStepsChanged(IBasicExecutionEngine engine,
-			Collection<LogicalStep> logicalSteps) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean validateVariableValue(String threadName,
-			String variableName, String value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Object getVariableValue(String threadName, String stackName,
-			String variableName, String value) {
-		// TODO Auto-generated method stub
+	protected String getBundleSymbolicName(LanguageDefinition languageDefinition) {
+		if (languageDefinition instanceof ConcurrentLanguageDefinition) {
+			return ((ConcurrentLanguageDefinition) languageDefinition).getDsaProject().getProjectName();
+		}
 		return null;
-	}
-
-	@Override
-	public void setVariableValue(String threadName, String stackName,
-			String variableName, Object value) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void engineAboutToDispose(IBasicExecutionEngine engine) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
