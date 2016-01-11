8fbd93468af3cee629a8b394ddee2381d71c0465
Fabien Coulon <fabien.coulon@inria.fr>
2016-01-11 11:14:17 +0100

    Forgotten renamming

--- ccsljava_execution/ccsljava_xdsml/plugins/org.gemoc.execution.concurrent.ccsljavaxdsml.ui/src/main/java/org/gemoc/execution/concurrent/ccsljavaxdsml/ui/dse/GemocDSENature.java
+++ ccsljava_execution/ccsljava_xdsml/plugins/org.gemoc.execution.concurrent.ccsljavaxdsml.ui/src/main/java/org/gemoc/execution/concurrent/ccsljavaxdsml/ui/dse/GemocDSENature.java
@@ -2,13 +2,14 @@ package org.gemoc.execution.concurrent.ccsljavaxdsml.ui.dse;
 
 import org.eclipse.core.runtime.CoreException;
 import org.gemoc.commons.eclipse.core.resources.AbstractProjectNature;
+import org.gemoc.execution.concurrent.ccsljavaxdsml.ui.Activator;
 
 public class GemocDSENature extends AbstractProjectNature {
 
 	/**
 	 * ID of this project nature
 	 */
-	public static final String NATURE_ID = "org.gemoc.gemoc_language_workbench.concurrent.ui.dse_nature";
+	public static final String NATURE_ID = Activator.PLUGIN_ID+".dse_nature";
 
 	@Override
 	public void configure() throws CoreException {
