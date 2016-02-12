dda521283ecce24dc13d4e89d060c599ff0dc002
Fabien Coulon <fabien.coulon@inria.fr>
2016-02-12 14:18:47 +0100

    Add content assist for selecting ECL file in Melange editor

---
+++ ccsljava_execution/ccsljava_xdsml/plugins/org.gemoc.execution.concurrent.ccsljavaxdsml.ui/src/main/java/org/gemoc/execution/concurrent/ccsljavaxdsml/ui/editor/SelectEclProposal.java
@@ -0,0 +1,41 @@
+package org.gemoc.execution.concurrent.ccsljavaxdsml.ui.editor;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.dialogs.Dialog;
+import org.gemoc.commons.eclipse.core.resources.FileFinderVisitor;
+import org.gemoc.commons.eclipse.ui.dialogs.SelectSpecificFileDialog;
+
+import fr.inria.diverse.melange.ui.contentassist.IProposal;
+
+public class SelectEclProposal implements IProposal{
+	
+	class SelectEclIFileDialog extends SelectSpecificFileDialog {
+		public FileFinderVisitor instanciateFinder() {
+			return new FileFinderVisitor("ecl");
+		}
+	}
+
+	@Override
+	public String getDisplayText() {
+		return "Select an existing ECL file";
+	}
+
+	@Override
+	public String getReplacementText() {
+		SelectEclIFileDialog dialog = new SelectEclIFileDialog();
+		if (dialog.open() == Dialog.OK) {
+			Object[] selections = dialog.getResult();
+			if(selections != null 
+				&& selections.length != 0
+				&& selections[0] instanceof IResource 
+			){
+				IResource eclFile = (IResource) selections[0];
+				String replacementText = "\"/"+eclFile.getProject().getName() +"/"+eclFile.getProjectRelativePath()+"\"";
+				return replacementText;
+			}
+		}
+		return "\"/project/file.ecore\"";
+	}
+
+}
