d64f1e5cfd22873f6e93227a72af86901cf985ed
Fabien Coulon <fabien.coulon@inria.fr>
2016-02-12 15:41:53 +0100

    Content assist: display GEMOC proposals first

--- ccsljava_execution/ccsljava_xdsml/plugins/org.gemoc.execution.concurrent.ccsljavaxdsml.ui/src/main/java/org/gemoc/execution/concurrent/ccsljavaxdsml/ui/editor/SelectEclProposal.java
+++ ccsljava_execution/ccsljava_xdsml/plugins/org.gemoc.execution.concurrent.ccsljavaxdsml.ui/src/main/java/org/gemoc/execution/concurrent/ccsljavaxdsml/ui/editor/SelectEclProposal.java
@@ -18,7 +18,7 @@ public class SelectEclProposal implements IProposal{
 
 	@Override
 	public String getDisplayText() {
-		return "Select an existing ECL file";
+		return "-- Select an existing ECL file --";
 	}
 
 	@Override
