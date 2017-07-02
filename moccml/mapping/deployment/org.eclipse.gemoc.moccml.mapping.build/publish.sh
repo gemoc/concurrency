5f19e25ff608953b33cd59f70eb9c1260950dffd
Julien Deantoni <julien.deantoni@inria.fr>
2017-07-02 15:25:47 +0200

    update MoCCML mapping update site

--- moccml/mapping/deployment/org.eclipse.gemoc.moccml.mapping.build/publish.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-PUBLISH_LOCATION=/home/groups/timesquare/htdocs/update/P1vhZHjAUIroD12YS
-
-ssh scm.gforge.inria.fr mkdir -p ${PUBLISH_LOCATION}
-scp -r build/buildRepo/* scm.gforge.inria.fr:${PUBLISH_LOCATION}
